5b54d666967720701be02e08b7c1565c06cb8549
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2021-03-05 16:30:56 +0800

    Add files via upload

---
+++ Insertionsort.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+
+int *Insertionsort(int arr[], int size) {
+	//int size = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 1; i < size; i++) {
+		int key = arr[i];
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j -= 1;
+		}
+		arr[j + 1] = key;
+	}
+	return arr;
+}
+
+int main()
+{
+	int arr[6] = { 4, 6, 5, 2, 3, 1 };
+	int *ptr = Insertionsort(arr, 6);
+	for (int i = 0; i < 6; i++) {
+		cout << ptr[i] << " ";
+	}
+	cout << endl;
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
